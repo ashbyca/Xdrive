b58dccf0c2cd62a17b2bdb7c451b74beae7a35f1
Christopher Ashby <ashbyca@users.noreply.github.com>
2016-09-24 09:00:40 -0400

    Update visio_start.sh
    cleanup code

--- visio_start.sh
+++ visio_start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 #
-# Author: Christopher Ashby
-# Last Updated: 20140325
-# 
 # Sets up enviroment for x86 32Bit wine and MS Visio - upon success it will start the Microsoft Visio 2010!
 #
 # Enable x86 32Bit Wine Prefix
