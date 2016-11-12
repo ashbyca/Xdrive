cf0555d4f2934692659f1e7ab5ed9ad94070bafe
Christopher Ashby <ashbyca@users.noreply.github.com>
2016-11-12 07:52:53 -0500

    Update visio_start.sh
    added $username value for profile

--- visio_start.sh
+++ visio_start.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 #
+# Author: Christopher Ashby
+# Last Updated: 20161112
+# 
 # Sets up enviroment for x86 32Bit wine and MS Visio - upon success it will start the Microsoft Visio 2010!
 #
+# Set Username Variable which sets the $Path
+u="$USER"
+echo "User name $u"
+
 # Enable x86 32Bit Wine Prefix
-export WINEPREFIX=/Users/[username]/.wine-visio
+export WINEPREFIX=/Users/$u/.visio
 
 # Enable archecture for 32Bit Wine 
 export WINEARCH=win32
 
 # Start Microsoft Visio 2010
-/usr/local/bin/wine ~/.wine-visio/drive_c/Program\ Files/Microsoft\ Office/Office14/VISIO.EXE
-
+/usr/local/bin/wine ~/.visio/drive_c/Program\ Files/Microsoft\ Office/Office14/VISIO.EXE
